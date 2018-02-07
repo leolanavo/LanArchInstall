dddac8e7c841842b7cde9ace8a7a0f7328a8b418
Leonardo Lana <leolanavo@gmail.com>
2018-02-06 23:45:53 -0200

    Remove the AUR installation from packages.json

--- packages_install.sh
+++ packages_install.sh
@@ -33,33 +33,3 @@ pacman -S termite
 # Enablem both Network Manager and Lightdm
 systemctl enable NetworkManager.service
 systemctl enable lightdm.service
-
-su $1
-
-# Import keys for discord app, because they aren`t importing
-# properl
-gpg --recv-key 8F0871F202119294
-gpg --lsign 8F0871F202119294
-
-# AUR packages
-yaourt -S google-chrome spotify gnome-software \
-          polybar i3-gaps \
-          lightdm-webkit2-greeter \
-          sublime-text-dev neofetch light discord \
-          lightdm-webkit2-theme-material2 \
-          opera-ffmpeg-codecs bash-pipes\
-          --noconfirm
-
-# The neovim-symlinks will try to remove the vi package, so I can`t
-# use --noconfirm option
-yaourt -S neovim-symlinks
-
-exit
-
-# Change the greeter for lightdm to webkit2-greeter
-sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter/' \
-	/etc/lightdm/lightdm.conf
-
-# Change the theme of the webkit2 to material2
-sed -i 's/webkit_theme        = antergos/webkit_theme        = material2/' \
-	/etc/lightdm/lightdm-webkit2-greeter.conf
