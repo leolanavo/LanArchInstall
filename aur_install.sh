6770ffaf5047c6e8d95c4261c62ec528e8c512c6
Leonardo Lana <leolanavo@gmail.com>
2018-10-18 08:35:04 -0300

    Migrate from aurman to yay and remove non-aur packages
    Signed-off-by: Leonardo Lana <leolanavo@gmail.com>

--- aur_install.sh
+++ aur_install.sh
@@ -2,15 +2,14 @@
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+cd ..
+rm -rf yay/
 
 # AUR packages
-yay -S google-chrome spotify gnome-software \
-          polybar i3-gaps-next-git \
-          lightdm-webkit2-greeter \
-          sublime-text-dev neofetch light \
+yay -S google-chrome spotify \
+          polybar i3-gaps light \
           lightdm-webkit2-theme-material2 \
           bash-pipes tty-clock fetchmirrors \
-          telegram-desktop-bin visual-studio-code-bin \
           ttf-montserrat otf-cooper-hewitt \
           --noconfirm
 
@@ -21,12 +20,6 @@ yay -S neovim-symlinks
 # This installation occurs here, because of its dependencies
 sudo pacman -S i3lock
 
-# Change the greeter for lightdm to webkit2-greeter
-sudo sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter/' \
-	/etc/lightdm/lightdm.conf
-
 # Change the theme of the webkit2 to material2
 sudo sed -i 's/webkit_theme        = antergos/webkit_theme        = material2/' \
 	/etc/lightdm/lightdm-webkit2-greeter.conf
-
-sudo systemctl enable -f lightdm.service
