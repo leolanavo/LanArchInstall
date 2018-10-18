d7eae726a30a645a3a47f6362504ca3ee6771806
Leonardo Lana <leolanavo@gmail.com>
2018-10-18 08:35:24 -0300

    Modularize script download
    Signed-off-by: Leonardo Lana <leolanavo@gmail.com>

--- packages_install.sh
+++ packages_install.sh
@@ -13,21 +13,55 @@ elif [ $1 == "pc" ]; then
     pacman -S nvidia nvidia-libgl lib32-nvidia-libgl --noconfirm
 fi
 
-pacman -S xorg-server xorg-xinit xorg-twm xorg-xclock xterm xclip xorg-xrandr xorg-xinput \
-          wpa_supplicant dialog networkmanager openssh network-manager-applet \
-          ntp intel-ucode pulseaudio alsa-utils \
-          gnome gdm gnome-extra gnome-tweak-tool lightdm \
-          file-roller neovim htop evince ranger compton terminator \
-          zsh feh maim sxiv mpd pavucontrol wget cmatrix atril \
-          emacs gimp inkscape rofi vlc scrot imagemagick\
-          texstudio texlive-core texlive-lang \
-          python3 ruby nodejs npm go go-tools crystal shards \
-          opera numlockx opera-ffmpeg-codecs docker docker-compose \
-          docker-machine yarn arc-gtk-theme arc-icon-theme lxappearance \
-          --noconfirm
+# Audio
+pacman -S pulseaudio alsa-utils mpd pavucontrol --noconfirm
+
+# Wifi
+pacman -S wpa_supplicant dialog networkmanager network-manager-applet --noconfirm
+
+# Base
+pacman -S zsh openssh wget intel-ucode ntp htop terminator --noconfirm
+
+# Xorg
+pacman -S xorg-server xorg-xinit xorg-twm \
+    xorg-xclock xterm xclip xorg-xrandr \
+    xorg-xinput --noconfirm
+
+# Editors
+pacman -S neovim code --noconfirm
+
+# Images
+pacman -S feh maim sxiv gimp inkscape scrot imagemagick --noconfirm
+
+# LaTeX
+pacman -S texstudio texlive-core texlive-lang --noconfirm
+
+# Docker
+pacman -S docker docker-compose docker-machine --noconfirm
+
+# Langs
+pacman -S python3 python-pip ruby nodejs npm go go-tools crystal shards yarn --noconfirm
+
+# Browser
+pacman -S opera opera-ffmpeg-codecs --noconfirm
+
+# Display Managers
+pacman -S gnome gnome-extra gnome-tweak-tool i3-gaps \
+    lightdm lightdm-webkit2-greeter --noconfirm
+
+# Utils
+pacman -S file-roller numlockx lxappearance \
+    compton rofi arc-gtk-theme arc-icon-theme \
+    evince cmatrix atril vlc telegram-desktop \
+    neofetch --noconfirm
 
 # Enablem both Network Manager and Lightdm
 systemctl enable NetworkManager.service
-systemctl enable gdm.service
+systemctl enable lightdm.service
 systemctl enable docker.socket
 systemctl enable docker.service
+
+# Change the greeter for lightdm to webkit2-greeter
+sed -i \
+    's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter/' \
+	/etc/lightdm/lightdm.conf
